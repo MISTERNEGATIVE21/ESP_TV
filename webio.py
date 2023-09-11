aad6044e57c2f9513ada27f44e5a8fefb6abca41
MISTER_NEGATIVE <supergogetavegito21@gmail.com>
2023-09-11 19:57:43 +0530

    frame rates changed

--- webio.py
+++ webio.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, Response
 from geventwebsocket.handler import WebSocketHandler
 from gevent.pywsgi import WSGIServer
 import cv2
+import time  # Import the time module
 
 app = Flask(__name__)
 
@@ -10,12 +11,18 @@ video_path = './test.mp4'
 
 cap = cv2.VideoCapture(video_path)
 
+# Define the desired FPS
+desired_fps = 15
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 def generate_frames():
     while True:
+        # Get the start time for measuring FPS
+        start_time = time.time()
+        
         ret, frame = cap.read()
         if not ret:
             break
@@ -29,6 +36,13 @@ def generate_frames():
 
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+        # Calculate the time taken for processing the frame
+        elapsed_time = time.time() - start_time
+
+        # Calculate the delay required to achieve the desired FPS
+        delay = max(0, 1 / desired_fps - elapsed_time)
+        time.sleep(delay)
 
 @app.route('/video_stream')
 def video_stream():
